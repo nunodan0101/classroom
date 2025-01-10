5162b494ee7451c7979115a4ad7a844074341814
nunokawa <nunodan0101@gmail.com>
2025-01-10 21:01:58 +0900

    条件文の修正

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ const { config } = require('dotenv');
 app.use('/', classroomRoutes);
 
 // ==== 4) サーバー起動 ====
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port `);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
